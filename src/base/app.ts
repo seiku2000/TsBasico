393c9a90b1a50e70f8bf86b8ce31afabb022b5d1
seiku2000 <eslumter@gmail.com>
2026-03-21 01:16:06 +0000

    tipos datos

---
+++ src/base/app.ts
@@ -0,0 +1,15 @@
+const msg:string = 'hola de nuevo';
+console.log(typeof(msg));
+
+const heroe = {
+    nombre: 'Iroman',
+    edad: 45
+}
+
+
+
+
+
+console.log(heroe.edad);
+console.log(heroe.edad += 1);
+console.log(heroe)
