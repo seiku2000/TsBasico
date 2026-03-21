128321bbe4ad452619b5e95c44fc0cf59afc21c2
seiku2000 <eslumter@gmail.com>
2026-03-21 02:16:13 +0000

    tipodedatos

---
+++ src/base/tiposDatos.ts
@@ -0,0 +1,12 @@
+const a:number=10;
+let b:string = 'hola';
+console.log(a);
+console.log(b);
+const mensaje:string = 'hola como estas de nuevo';
+
+
+function   sayHello(msg:string):void{
+    console.log(msg);
+}
+
+sayHello(mensaje);
