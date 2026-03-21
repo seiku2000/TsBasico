bb14f7961e8d6b655dfdc04914aacea916d52f85
seiku2000 <eslumter@gmail.com>
2026-03-21 02:16:01 +0000

    booleans

---
+++ src/base/booleans.ts
@@ -0,0 +1,7 @@
+let isSuperman:boolean = true;
+let isBatmant:boolean = false;
+console.log(isSuperman);
+console.log(isBatmant);
+//isBatmant = true && false;
+isSuperman =(isBatmant) ? true :false;
+console.log(isSuperman);
