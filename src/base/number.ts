7b164405ce8708a62a216242c34e3cd69552047d
seiku2000 <eslumter@gmail.com>
2026-03-21 19:00:25 +0000

    numbers

---
+++ src/base/number.ts
@@ -0,0 +1,12 @@
+let avengers:number =5;
+console.log(avengers);
+
+const villians:number = 8;
+if(avengers < villians){
+    console.log('estamos problemas');
+}else {
+    console.log('estamos a salvo');
+}
+avengers  =Number('55a');
+console.log(avengers);
+
