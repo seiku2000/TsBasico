a1a79c4a8e9a58744165b479a0eb711653ad48c3
seiku2000 <eslumter@gmail.com>
2026-03-22 00:20:35 +0000

    string

---
+++ src/base/string.ts
@@ -0,0 +1,16 @@
+const batman = 'batman';
+const linternaVerde = "linterna verde";
+const volcanNegro  =`heroe:Volcan negro`;
+
+console.log(batman,
+    linternaVerde,
+    volcanNegro
+);
+
+console.log(`i am ${batman}`);
+console.log(batman.toUpperCase());
+console.log(batman.toLowerCase());
+console.log(batman[10]?.toUpperCase() 
+|| 'no esta presente'
+
+)
