d6ed40ade462e2597df1a05986b21eebe7d63e30
seiku2000 <eslumter@gmail.com>
2026-04-07 17:39:54 +0000

    any

---
+++ src/base/any.ts
@@ -0,0 +1,15 @@
+let avenger:any = 123 ;
+let exits;
+let power;
+
+avenger = 'dr little';
+console.log((avenger as string ).charAt(0));
+
+avenger = 150.23234;
+//es una forma de castiar a un numero a string, pero no es recomendable
+//console.log((<number>avenger).toFixed(2));
+
+console.log(exits);
+console.log(power);
+
+
