90f3f4a5449bfcc5d252a5737ee8ad3c367da078
seiku2000 <eslumter@gmail.com>
2026-04-07 17:39:45 +0000

    arrays

---
+++ src/base/arrays.ts
@@ -0,0 +1,11 @@
+//const numbers:(number | string | boolean)[]  = [1, 2,'a', 4, 5];
+const numbers:(number )[]  = [1, 2,3, 4, 5];
+console.log(numbers);
+const villanos = ['Omega Rojo', 'Dormammu', 'Duende Verde'];    
+villanos.forEach(villano => console.log(villano.toUpperCase()));
+
+
+
+//numbers.push(11);
+//numbers.push(true);
+//console.log(numbers);
