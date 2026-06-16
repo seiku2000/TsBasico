b52d61348e45301457a347de8a6dd59d44de173b
seiku2000 <eslumter@gmail.com>
2026-06-16 20:15:55 +0000

    basico cambios

--- src/base/app.ts
+++ src/base/app.ts
@@ -1,7 +1,24 @@
 const a:number= 10;
-let b:number  = 20;
+let b:string;
+b="hola como estas";
 
+function  sayHello(msg:string):void {
+    console.log(msg);
+
+}
+const sayhello2 = () => {
+   let b= "hola como estas de nuevo";
+    return b;
+}
 console.log(a);
-console.log(b);
+sayHello(b);
 
-console.log(`este es el valor ${a} y este valor es ${b}`);
+console.log(sayhello2())    
+
+//console.log(a);
+//console.log(b);
+
+//console.log(`este es el valor ${a} y este valor es ${b}`);
+
+
+
