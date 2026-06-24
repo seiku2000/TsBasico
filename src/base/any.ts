153794185f64172cf04162fbb33a0b18a09371db
seiku2000 <eslumter@gmail.com>
2026-06-24 14:10:32 -0700

    Cambios en main.ts y any.ts

--- src/base/any.ts
+++ src/base/any.ts
@@ -1,15 +1,13 @@
-let avenger:any = 123 ;
-let exits;
+let avenger:any = 123;
+const exists = false;
 let power;
 
-avenger = 'dr little';
-console.log((avenger as string ).charAt(0));
+avenger = "dr . strange";
+console.log((avenger as string).charAt(0));
+//forma de castiar de un any a un string
+avenger = 150.23244343;
+console.log((avenger as number).toFixed(2 )); 
+//forma hacer casting de un any a un number
 
-avenger = 150.23234;
-//es una forma de castiar a un numero a string, pero no es recomendable
-//console.log((<number>avenger).toFixed(2));
-
-console.log(exits);
-console.log(power);
-
-
+console.log({exists});
+console.log({power});
