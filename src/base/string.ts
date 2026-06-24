03484197b2ff67067c6182a7262a4ae7ef8d86b5
seiku2000 <eslumter@gmail.com>
2026-06-24 13:41:16 -0700

    Cambios en string.ts y main.ts

--- src/base/string.ts
+++ src/base/string.ts
@@ -1,16 +1,25 @@
-const batman = 'batman';
-const linternaVerde = "linterna verde";
+const batman:string = 'batman';
+const linternaVerde:string = "linterna verde";
 const volcanNegro  =`heroe:Volcan negro`;
+
+
+
+console.log(`i am ${batman}`);
+console.log(batman.toUpperCase());
+console.log(batman.toLowerCase());
+console.log(batman[10]?.toUpperCase() || 'no esta presente');
+
+
 
 console.log(batman,
     linternaVerde,
     volcanNegro
 );
-
+/*
 console.log(`i am ${batman}`);
 console.log(batman.toUpperCase());
 console.log(batman.toLowerCase());
 console.log(batman[10]?.toUpperCase() 
 || 'no esta presente'
 
-)
+)*/
