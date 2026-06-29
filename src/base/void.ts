d6b42f44b9591226f53a5f213143aca68056afde
seiku2000 <eslumter@gmail.com>
2026-06-29 12:44:35 -0700

    Add void type example

---
+++ src/base/void.ts
@@ -0,0 +1,17 @@
+//const hola = "hola";
+
+
+function callBatman():void {
+
+return;
+}
+//puede recibir un valor pero no puede regresar un valor
+const callSupertman = ():void=> {
+   // return  true no permite regresar un valor
+
+}
+
+
+
+const a = callBatman()
+console.log(a);
