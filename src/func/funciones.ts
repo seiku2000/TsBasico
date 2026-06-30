a67edb88d058b2e06def0da2e7ab73b901db4820
seiku2000 <eslumter@gmail.com>
2026-06-29 18:09:49 -0700

    funciones

---
+++ src/func/funciones.ts
@@ -0,0 +1,17 @@
+const hero:string = "flash";
+
+function returnName():string {
+
+    return hero;
+}
+
+const activateBatiSignal = ():string => {
+
+    return "batiseñal activada ";
+    
+}
+
+console.log(typeof activateBatiSignal);
+
+const heroName = returnName();
+console.log(heroName);
