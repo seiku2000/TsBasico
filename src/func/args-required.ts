b44ac28c32d471ede6394121da5c44f9d3a90dc6
seiku2000 <eslumter@gmail.com>
2026-06-29 21:42:28 -0700

    argsrequeried

--- src/func/args-required.ts
+++ src/func/args-required.ts
@@ -1,3 +1,7 @@
+
+//aqui los argumentos son obligatorios
 const fullName =(firstName:string,lastName:string):string => {
     return `${firstName} ${lastName}`;
 }
+const name = fullName("john","done");
+console.log(name);
