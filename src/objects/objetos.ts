ab91b2dc7e6ecd4d6299c7c4566a3cdf6a0b4138
seiku2000 <eslumter@gmail.com>
2026-07-02 12:05:24 -0700

    objetos

---
+++ src/objects/objetos.ts
@@ -0,0 +1,52 @@
+
+
+let flash:{name:string,age?:number,powers:string[],
+    getNombre?:() => string
+} = {
+    name: 'Barry Allen',
+    age:25,
+    powers: ['Super speed', 'Time travel']
+
+}
+
+
+let superman:{name:string,age?:number,powers:string[],
+    getNombre?:() => string
+} = {
+    name: 'Clark Kent',
+    age:30,
+    powers: ['Super strength', 'Flight']
+}
+/*
+flash ={
+    name: 'otro nombre',
+  //  age: 30,
+    powers: ['Super speed'],
+    getNombre(){
+        return this.name;
+    }
+}
+
+
+
+console.log(flash);
+console.log(flash.getNombre?.());
+*/
+//const { name, age, powers } = flash;
+
+
+
+
+
+/*
+flash ={
+    name: 'otro nombre',
+    age: 30,
+    powers: ['Super speed', 'Time travel']
+}
+/*
+let flashAdv:{
+     name:string,
+        age:number,
+        powers:string[],
+}*/
