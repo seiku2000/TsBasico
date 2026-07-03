62ccce65180d9864ee2291fe447ba43b4c6bbafc
seiku2000 <eslumter@gmail.com>
2026-07-02 19:45:05 -0700

    let

---
+++ src/es6/letvar.ts
@@ -0,0 +1,15 @@
+const nombre = "jose";
+
+/*
+function getName(){
+
+}*/
+
+let nombreusuario;
+const getName = () =>{
+ console.log('viejo name');
+} 
+nombreusuario = getName();
+
+
+console.log(nombreusuario);
